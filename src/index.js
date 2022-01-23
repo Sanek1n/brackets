a9c372f64e10a399e2d35e222b2d425a0b4f3930
Sanek1n <sanekin8ru@outlook.com>
2022-01-23 20:15:22 +0400

    fix: fixed function finInArray

--- src/index.js
+++ src/index.js
@@ -1,35 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
 
   function findInArray(findString, placeFind) { //фунция поиска значения в массиве
-    return placeFind.reduce((index,arr) => {
-      let placeElement = arr.findIndex((val) => val === findString);
-      if ( placeElement > -1) {
-        index = placeElement; 
-      } 
-      return index
-  }, -1)
+    let findElement 
+    placeFind.forEach((value) => {
+      if (value[0] === findString) {
+        findElement = value[1];
+      }
+    })
+    return findElement;
   };
 
-  let key = 0;
   let array = [];
-  let arrayBracket = bracketsConfig.map(val => val[0] + val[1]);
-
-  for (let i=0; i < str.length; i++) {
-    //console.log(array, str[i])
-    if (findInArray(str[i],bracketsConfig) === 0) {
-      array.push(str[i]);
-    } else if (findInArray(str[i],bracketsConfig) === 1) {
-      
-      if (arrayBracket.indexOf(String(array[array.length -1] + str[i])) === -1) {
-
-        return false;
-
-      } else {
+    for (let i=0; i < str.length; i++) {
+    if (findInArray(array[array.length-1], bracketsConfig) == str[i]) {
         array.pop();
+      } else {
+        array.push(str[i]);
       }
-    } 
-  }
-  //console.log(array.length);
+    }
     if (array.length === 0) {
       return true;
     } else {
